9456ddca75d7fa075187d6bbfd46ea9fc2806ca6
Dano <danielmejialban@gmail.com>
2023-05-21 18:09:45 +0200

    Add: all the core features

---
+++ src/app/services/contacts.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from '../types/contact.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+
+  private readonly urlBase: string = 'http://localhost:8080/contacts';
+
+  constructor(private readonly http: HttpClient  ) { }
+
+  public getContacts() : Observable<Contact[]> {
+    return this.http.get<Contact[]>(`${this.urlBase}/getAll`);
+  }
+
+  public getContactById(id: number): Observable<Contact> {
+    return this.http.get<Contact>(`${this.urlBase}/${id}`);
+  }
+
+  public addContact(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(`${this.urlBase}/add`, contact);
+  }
+
+  public deleteContact(id: number|undefined): Observable<Contact> {
+    return this.http.delete<Contact>(`${this.urlBase}/${id}`);
+  }
+
+  public updateContact(id: number, contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.urlBase}/${id}`, contact);
+  }
+
+}
---
+++ src/app/store/contacts.effects.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { switchMap, map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { ContactsService } from '../services/contacts.service';
+import {
+  loadContacts,
+  loadContactsSuccess,
+  loadContactsFailure,
+  addContact,
+  addContactSuccess,
+  addContactFailure,
+  updateContact,
+  updateContactSuccess,
+  updateContactFailure,
+  deleteContact,
+  deleteContactSuccess,
+  deleteContactFailure
+} from './contacts.actions';
+
+@Injectable()
+export class ContactsEffects {
+  constructor(private readonly actions$: Actions, private readonly contactsService: ContactsService) {}
+
+  loadContacts$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadContacts),
+      switchMap(() =>
+        this.contactsService.getContacts().pipe(
+          map(contacts => loadContactsSuccess({ contacts })),
+          catchError(error => of(loadContactsFailure({ error })))
+        )
+      )
+    )
+  );
+
+  addContact$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(addContact),
+      switchMap(action =>
+        this.contactsService.addContact(action.contact).pipe(
+          map(contact => addContactSuccess({ contact })),
+          catchError(error => of(addContactFailure({ error })))
+        )
+      )
+    )
+  );
+
+  updateContact$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(updateContact),
+      switchMap(action =>
+        this.contactsService.updateContact(action.id,action.contact).pipe(
+          map(contact => updateContactSuccess({ contact })),
+          catchError(error => of(updateContactFailure({ error })))
+        )
+      )
+    )
+  );
+
+  deleteContact$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(deleteContact),
+      switchMap(action =>
+        this.contactsService.deleteContact(action.id).pipe(
+          map(() => deleteContactSuccess({ id: action.id })),
+          catchError(error => of(deleteContactFailure({ error })))
+        )
+      )
+    )
+  );
+}
